3c6a7154ea8b6ab3dfc73110c9af8e5b981b5921
runner-harris <harriswh@g.cofc.edu>
2022-04-04 11:13:15 -0400

    Wall Jump and Attack

---
+++ WeemsRamen/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float coolDownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0) && coolDownTimer > attackCooldown && playerMovement.canAttack())
+            Attack();  
+
+        coolDownTimer += Time.deltaTime;    
+    }
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        coolDownTimer = 0;
+
+        //pool fireball
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    private int FindFireball()
+    {
+        for( int i = 0; i < fireballs.Length; i++)
+        {
+            if(!fireballs[i].activeInHierarchy)
+                return i;
+        }
+
+        return 0;
+    }
+}
